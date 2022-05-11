288b9fc7e477751733189d110b7ff739713bc5ff
LauraWaen <laurawaenerberg@gmail.com>
2022-05-11 16:30:36 +0300

    Lopputyön ensimmäinen kohta

---
+++ src/Home.js
@@ -0,0 +1,30 @@
+import React from "react";
+import Home from ".//Home";
+import Customerlist from ".//components/Customerlist";
+import Traininglist from ".//components/Traininglist";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+
+const Etusivu = () => {
+	return (
+		<div className="container py-5">
+			<center><h1>Welcome to Personal Trainer page!</h1></center>
+			<p>
+				<div>
+			<BrowserRouter>
+			<center>
+            <Link to="/">Home</Link>{' '}
+            <Link to="/components/Customerlist">Customers</Link>{' '} 
+			<Link to="/Traininglist">Trainings</Link>{' '} </center>
+					<Routes>
+						<Route exact path="/components/" element={<Home />} />
+						<Route exact path="/components/Customerlist" element={<Customerlist />} />
+						<Route exact path="/Traininglist" element={<Traininglist />} />
+					</Routes>
+			</BrowserRouter>
+		</div>
+			</p>
+		</div>
+	);
+};
+
+export default Etusivu;
